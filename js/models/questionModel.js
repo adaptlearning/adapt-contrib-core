02a35ce9dbdd481bbd1fe30b27785df403d6857d
Oliver Foster <oliver.foster@kineo.com>
2021-11-09 14:41:55 +0000

    issue/41 Allow handlebars model properties in feedback

--- js/models/questionModel.js
+++ js/models/questionModel.js
@@ -244,7 +244,7 @@ class QuestionModel extends ComponentModel {
   setupCorrectFeedback() {
     this.set({
       feedbackTitle: this.getFeedbackTitle(),
-      feedbackMessage: this.get('_feedback').correct
+      feedbackMessage: Handlebars.compile(this.get('_feedback').correct)(this.toJSON())
     });
   }
 
@@ -267,12 +267,13 @@ class QuestionModel extends ComponentModel {
 
     this.set({
       feedbackTitle: this.getFeedbackTitle(),
-      feedbackMessage: body
+      feedbackMessage: Handlebars.compile(body)(this.toJSON())
     });
   }
 
   getFeedbackTitle() {
-    return this.get('_feedback').title || this.get('displayTitle') || this.get('title') || '';
+    const title = this.get('_feedback').title || this.get('displayTitle') || this.get('title') || '';
+    return Handlebars.compile(title)(this.toJSON());
   }
 
   /**
