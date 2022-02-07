c29eacee699499b4bca50b550f12c009e0d9b2ee
Oliver Foster <oliver.foster@kineo.com>
2022-02-07 09:53:03 +0000

    issue/3281 Added data-order to buttons for sorting navigation (#61)

--- js/views/navigationView.js
+++ js/views/navigationView.js
@@ -14,15 +14,16 @@ class NavigationView extends Backbone.View {
 
   attributes() {
     return {
-      'role': 'navigation'
+      role: 'navigation'
     };
   }
 
   initialize() {
+    this.sortNavigationButtons = _.debounce(this.sortNavigationButtons.bind(this), 1);
     this.listenToOnce(Adapt, {
       'courseModel:dataLoading': this.remove
     });
-    this.listenTo(Adapt, 'router:menu router:page', this.hideNavigationButton);
+    this.listenTo(Adapt, 'router:menu router:page', this.onNavigate);
     this.preRender();
   }
 
@@ -72,9 +73,22 @@ class NavigationView extends Backbone.View {
     Adapt.a11y.focusFirst('.' + Adapt.location._contentType);
   }
 
+  onNavigate(model) {
+    this.hideNavigationButton(model);
+    this.sortNavigationButtons();
+  }
+
   hideNavigationButton(model) {
     const shouldHide = (model.get('_type') === 'course');
     this.$('.nav__back-btn, .nav__home-btn').toggleClass('u-display-none', shouldHide);
+  }
+
+  sortNavigationButtons() {
+    const container = this.$('.nav__inner')[0];
+    const items = [...container.children];
+    items
+      .sort((a, b) => parseFloat($(a).attr('data-order') || 0) - parseFloat($(b).attr('data-order') || 0))
+      .forEach(item => container.appendChild(item));
   }
 
   showNavigationButton() {
