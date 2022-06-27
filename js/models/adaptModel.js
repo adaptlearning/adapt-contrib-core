c0a78b28d81720a5473f0c26d9c41c536f979691
Oliver Foster <oliver.foster@kineo.com>
2022-06-27 12:50:26 +0100

    issue/130 Filter _isTracking === false from trackingPosition API (#131)

--- js/models/adaptModel.js
+++ js/models/adaptModel.js
@@ -58,7 +58,8 @@ export default class AdaptModel extends LockingModel {
     const nearestTrackingIdModel = [firstDescendant].concat(firstDescendant.getAncestorModels()).find(model => model.has('_trackingId'));
     if (!nearestTrackingIdModel) return;
     const trackingId = nearestTrackingIdModel.get('_trackingId');
-    const trackingIdDescendants = [nearestTrackingIdModel].concat(nearestTrackingIdModel.getAllDescendantModels(true));
+    let trackingIdDescendants = [nearestTrackingIdModel].concat(nearestTrackingIdModel.getAllDescendantModels(true));
+    trackingIdDescendants = trackingIdDescendants.filter(model => !(model.isTypeGroup('component') && model.get('_isTrackable') === false));
     const indexInTrackingIdDescendants = trackingIdDescendants.findIndex(descendant => descendant === this);
     if (indexInTrackingIdDescendants >= 0) {
       // Is either the nearestTrackingIdModel (0) or one of its flattened descendants (>0)
