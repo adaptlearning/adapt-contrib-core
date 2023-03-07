25a6a1941c298959fbc25bc84dc4e0b6e137147d
danielghost <daniel.ghost@kineo.com>
2023-03-07 15:04:19 +0000

    New: Update tracking logic to account for Scoring API  (fixes #112)

--- js/tracking.js
+++ js/tracking.js
@@ -20,70 +20,36 @@ class Tracking extends Backbone.Controller {
   setupEventListeners() {
     this.listenTo(Adapt, {
       'assessment:complete': this.onAssessmentComplete,
-      'assessment:restored': this.onAssessmentRestored
+      'assessment:restored': this.onAssessmentRestored,
+      'scoring:complete': this.onScoringComplete,
+      'scoring:restored': this.onScoringRestored
     });
 
-    // Check if completion requires completing all content.
     if (this._config._requireContentCompleted) {
       this.listenTo(Adapt.course, 'change:_isComplete', this.checkCompletion);
     }
   }
 
-  /**
-   * Store the assessment state.
-   * @param {object} assessmentState - The object returned by Adapt.assessment.getState()
-   */
-  onAssessmentComplete(assessmentState) {
-    this._assessmentState = assessmentState;
-
-    this.submitScore();
-    // Check if completion requires passing an assessment.
-    if (this._config._requireAssessmentCompleted) {
-      this.checkCompletion();
-    }
-  }
-
   submitScore() {
     if (!this._config._shouldSubmitScore) return;
-
-    if (this._assessmentState.isPercentageBased) {
-      offlineStorage.set('score', this._assessmentState.scoreAsPercent, 0, 100);
-      return;
-    }
-
-    offlineStorage.set('score', this._assessmentState.score, this._assessmentState.minScore, this._assessmentState.maxScore);
+    offlineStorage.set('score', this._assessmentState.score, this._assessmentState.minScore, this._assessmentState.maxScore, this._assessmentState.isPercentageBased);
   }
 
   /**
-   * Restores the _assessmentState object when an assessment is registered.
-   * @param {object} assessmentState - An object representing the overall assessment state
-   */
-  onAssessmentRestored(assessmentState) {
-    this._assessmentState = assessmentState;
-  }
-
-  /**
-   * Evaluate the course and assessment completion.
+   * Evaluate the course and assessment completion
    */
   checkCompletion() {
     const completionData = this.getCompletionData();
-
-    if (completionData.status === COMPLETION_STATE.INCOMPLETE) {
-      return;
-    }
-    
-    const canRetry = (completionData.assessment?.canRetry === true);
-    if (completionData.status === COMPLETION_STATE.FAILED && canRetry) {
-      return;
-    }
-
+    if (completionData.status === COMPLETION_STATE.INCOMPLETE) return;
+    const canRetry = completionData.assessment?.canRetry;
+    if (completionData.status === COMPLETION_STATE.FAILED && canRetry) return;
     Adapt.trigger('tracking:complete', completionData);
     logging.debug('tracking:complete', completionData);
   }
 
   /**
-   * The return value of this function should be passed to the trigger of 'tracking:complete'.
-   * @returns An object representing the user's course completion.
+   * The return value of this function should be passed to the trigger of 'tracking:complete'
+   * @returns An object representing the user's course completion
    */
   getCompletionData() {
     const completionData = {
@@ -91,34 +57,20 @@ class Tracking extends Backbone.Controller {
       assessment: null
     };
 
-    // Course complete is required.
-    if (this._config._requireContentCompleted && !Adapt.course.get('_isComplete')) {
-      // INCOMPLETE: course not complete.
-      return completionData;
-    }
-
-    // Assessment completed required.
+    if (this._config._requireContentCompleted && !Adapt.course.get('_isComplete')) return completionData;
     if (this._config._requireAssessmentCompleted) {
-      if (!this._assessmentState) {
-        // INCOMPLETE: assessment is not complete.
-        return completionData;
-      }
-
-      // PASSED/FAILED: assessment completed.
-      completionData.status = this._assessmentState.isPass ? COMPLETION_STATE.PASSED : COMPLETION_STATE.FAILED;
+      if (!this._assessmentState) return completionData;
+      const isPassed = this._assessmentState?.isPass ?? this._assessmentState?.isPassed;
+      completionData.status = isPassed ? COMPLETION_STATE.PASSED : COMPLETION_STATE.FAILED;
       completionData.assessment = this._assessmentState;
-
       return completionData;
     }
-
-    // COMPLETED: criteria met, no assessment requirements.
     completionData.status = COMPLETION_STATE.COMPLETED;
-
     return completionData;
   }
 
   /**
-   * Set the _config object to the values retrieved from config.json.
+   * Set the _config object to the values retrieved from config.json
    */
   loadConfig() {
     this._config = Adapt.config.get('_completionCriteria') ?? this._config;
@@ -127,6 +79,27 @@ class Tracking extends Backbone.Controller {
     // If the legacy property exists, use it for backward compatibility but warn in the console
     if (legacyShouldSubmitScore !== undefined) logging.deprecated('config.json:_spoor._tracking._shouldSubmitScore, please use only config.json:_completionCriteria._shouldSubmitScore');
     this._config._shouldSubmitScore = legacyShouldSubmitScore ?? newShouldSubmitScore ?? true;
+  }
+
+  onAssessmentComplete(assessmentState) {
+    // stop listening to `scoring:complete` if using Scoring API assessment backward compatibility
+    this.stopListening(Adapt, 'scoring:complete', this.onScoringComplete);
+    this.onScoringComplete(assessmentState);
+  }
+
+  onScoringComplete(state) {
+    this._assessmentState = state;
+    this.submitScore();
+    if (this._config._requireAssessmentCompleted) this.checkCompletion();
+  }
+
+  onAssessmentRestored(assessmentState) {
+    this.onScoringRestored(assessmentState);
+  }
+
+  onScoringRestored(state) {
+    if (this._assessmentState) return;
+    this._assessmentState = state;
   }
 
 }
