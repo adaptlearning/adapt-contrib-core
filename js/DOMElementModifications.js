6147aeb59c009112c4553eb9a3ff60d63ad161ab
Oliver Foster <oliver.foster@kineo.com>
2023-04-26 21:22:57 +0100

    Combine classes into one file

--- js/DOMElementModifications.js
+++ js/DOMElementModifications.js
@@ -1,5 +1,32 @@
 import Backbone from 'backbone';
-import DOMElementModificationEventObject from './DOMElementModificationEventObject';
+
+export class DOMElementModificationEventObject {
+
+  /**
+   * This class represents the first argument of the added, removed and changed events
+   *
+   * @param {Object} options
+   * @param {string} options.type Event type, removed, added, changed
+   * @param {Element} options.target Element at which the event occured
+   * @param {Object} options.changedAttributes Attribute names and values after change
+   * @param {Object} options.previousAttributes Attribute names and values before change
+   */
+  constructor({
+    type,
+    target,
+    changedAttributes,
+    previousAttributes
+  } = {}) {
+    /** @type {string} Event type, removed, added, changed */
+    this.type = type;
+    /** @type {Element} Element at which the event occured */
+    this.target = target;
+    /** @type {Object} Attribute names and values after change */
+    this.changedAttributes = changedAttributes;
+    /** @type {Object} Attribute names and values before change */
+    this.previousAttributes = previousAttributes;
+  }
+}
 
 /**
  * Allows the capture of DOM descendant nodes on addition, removal or change.
