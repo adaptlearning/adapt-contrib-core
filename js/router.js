bccd280e5e4c9ea56fe9bc9452d66326bc6f1e92
Oliver Foster <oliver.foster@kineo.com>
2023-06-26 10:30:17 +0100

    Fix: Added _isRendered: true to main view render

--- js/router.js
+++ js/router.js
@@ -184,7 +184,10 @@ class Router extends Backbone.Router {
     const type = isCourse ? 'menu' : model.get('_type');
     const newLocation = isCourse ? 'course' : `${type}-${id}`;
 
-    model.set('_isVisited', true);
+    model.set({
+      _isVisited: true,
+      _isRendered: true
+    });
     await this.updateLocation(newLocation, type, id, model);
 
     Adapt.once('contentObjectView:ready', () => {
