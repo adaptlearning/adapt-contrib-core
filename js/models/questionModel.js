44234a1b389b2bdd2acea505345e1ccb2add366a
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2023-08-15 16:29:12 +0100

    Fix: remove component diplayTitle and Title fallbacks for feedbackTitle (fixes #411)
    * Fix: Feedback title to use altTitle if feedback title is empty.
    
    * Fix: remove diplayTitle and title fallback fof feedbackTitle.

--- js/models/questionModel.js
+++ js/models/questionModel.js
@@ -286,8 +286,6 @@ class QuestionModel extends ComponentModel {
         Adapt.course.get('_globals')._accessibility.altFeedbackTitle ||
         '',
       title: feedback.title ||
-        this.get('displayTitle') ||
-        this.get('title') ||
         '',
       _classes: feedback._classes,
       ...(isLegacyConfig
