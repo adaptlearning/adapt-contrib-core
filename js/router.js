16a49c7ebb8a55a6e0ee2cf6851b2a343f7e8e14
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2023-08-24 17:18:26 +0100

    Fix: focusFirst on document.body when navigating to a contentObject

--- js/router.js
+++ js/router.js
@@ -366,6 +366,10 @@ class Router extends Backbone.Router {
             resolve();
           }, 1)));
           this.isScrolling = false;
+          if (currentModel.isTypeGroup('contentobject')) {
+            a11y.focusFirst(document.body);
+            return;
+          }
         }
         await Adapt.parentView.renderTo(currentModelId);
       }
