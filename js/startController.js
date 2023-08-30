4aa59761038c1d5e44bb809742d625e8fec42d45
Dan Ghost <daniel.ghost@kineo.com>
2023-08-30 09:59:20 +0100

    Fix: trickle incorrectly unlocking when switching languages, plus minor code refactor (fixes #412). (#413)

--- js/startController.js
+++ js/startController.js
@@ -1,5 +1,4 @@
 import Adapt from 'core/js/adapt';
-import wait from 'core/js/wait';
 import LockingModel from 'core/js/models/lockingModel';
 import router from 'core/js/router';
 import data from 'core/js/data';
@@ -8,7 +7,16 @@ class StartController extends Backbone.Controller {
 
   initialize(...args) {
     super.initialize(...args);
+    this._isSessionInProgress = false;
     this.model = null;
+    this.setupListeners();
+  }
+
+  setupListeners() {
+    this.listenTo(Adapt, {
+      'adapt:start': this.onAdaptStart,
+      'navigation:returnToStart': this.returnToStartLocation
+    });
   }
 
   loadCourseData() {
@@ -16,10 +24,18 @@ class StartController extends Backbone.Controller {
   }
 
   setStartLocation() {
-    if (!this.isEnabled()) return;
-    window.history.replaceState('', '', this.getStartHash());
+    if (!this._isSessionInProgress) {
+      this._isSessionInProgress = true;
+      if (!this.isEnabled()) return;
+      return window.history.replaceState('', '', this.getStartHash());
+    }
+    const hash = this.isEnabled() ? this.getStartHash(false) : '#/';
+    router.navigate(hash, { trigger: true, replace: true });
   }
 
+  /**
+   * Called via `Adapt.trigger('navigation:returnToStart')` or by including a button in the top navigation bar with the attribute `data-event="returnToStart"`
+   */
   returnToStartLocation() {
     const startIds = this.model.get('_startIds');
     if (startIds) {
@@ -39,11 +55,7 @@ class StartController extends Backbone.Controller {
     const isRouteSpecified = window.location.href.indexOf('#') > -1;
     const shouldForceStartId = alwaysForce || this.model.get('_force');
     const shouldNavigateToStartId = startId && (!isRouteSpecified || shouldForceStartId);
-
-    if (shouldNavigateToStartId && startId !== Adapt.course.get('_id')) {
-      return '#/id/' + startId;
-    }
-
+    if (shouldNavigateToStartId && startId !== Adapt.course.get('_id')) return `#/id/${startId}`;
     // If there's a route specified in location.hash, use that - otherwise go to main menu
     return window.location.hash || '#/';
   }
@@ -55,59 +67,33 @@ class StartController extends Backbone.Controller {
   getStartId() {
     let startId = this.model.get('_id');
     const startIds = this.model.get('_startIds');
-
     if (!startIds?.length) return startId;
-
     const $html = $('html');
     for (let i = 0, l = startIds.length; i < l; i++) {
       const item = startIds[i];
       const className = item._className;
       const skipIfComplete = item._skipIfComplete;
-
       const model = data.findById(item._id);
-
       if (!model) {
         console.log('startController: cannot find id', item._id);
         continue;
       }
-
-      if (skipIfComplete) {
-        if (model.get('_isComplete')) continue;
-      }
-
+      if (skipIfComplete && model.get('_isComplete')) continue;
       if (!className || $html.is(className) || $html.hasClass(className)) {
         // See https://github.com/adaptlearning/adapt_framework/issues/1843
         startId = item._id;
         break;
       }
     }
-
     return startId;
   }
 
+  onAdaptStart() {
+    this.loadCourseData();
+    this.setStartLocation();
+  }
+
 }
-
-Adapt.once('adapt:start', () => {
-  startController.loadCourseData();
-  startController.setStartLocation();
-});
-
-/*
-* allows you to call returnToStartLocation either by calling `Adapt.trigger('navigation:returnToStart')`
-* or by including in the top navigation bar a button that has the attribute `data-event="returnToStart"`
-*/
-Adapt.on('navigation:returnToStart', () => {
-  startController.returnToStartLocation();
-});
-
-Adapt.on('app:languageChanged', () => {
-  wait.for(done => {
-    startController.loadCourseData();
-    const hash = startController.isEnabled() ? startController.getStartHash(false) : '#/';
-    router.navigate(hash, { trigger: true, replace: true });
-    done();
-  });
-});
 
 const startController = new StartController();
 export default startController;
