bea023832e92adf063e67695c26c69116a4f5ece
danielghost <daniel.ghost@kineo.com>
2025-06-24 21:45:09 +0100

    Resize the parent element when fullscreen has been requested for an element. Prevents the `window.scrollY` shifting, potentially altering the inview and onscreen measurements and any functionality hooked into these.

--- js/fixes.js
+++ js/fixes.js
@@ -1,3 +1,4 @@
 import 'core/js/fixes/img.lazyload';
 import 'core/js/fixes/reactHelpers.html';
 import 'core/js/fixes/safari.label.click.blur';
+import 'core/js/fixes/fullscreen.parent.resize';
---
+++ js/fixes/fullscreen.parent.resize.js
@@ -0,0 +1,44 @@
+function overrideFullscreenRequest(name) {
+  const original = HTMLElement.prototype[name];
+  if (!original) return;
+  HTMLElement.prototype[name] = function(...args) {
+    const event = new CustomEvent('preRequestFullscreen', { bubbles: true });
+    this.dispatchEvent(event);
+    original.call(this, ...args);
+  };
+}
+
+[
+  'requestFullscreen',
+  'webkitRequestFullscreen',
+  'webkitRequestFullScreen',
+  'mozRequestFullScreen'
+].forEach(overrideFullscreenRequest);
+
+function onPreRequestFullscreen(event) {
+  if (document.fullscreenElement) return;
+  try {
+    const target = event.target;
+    const parent = target?.parentElement;
+    if (!parent) return;
+    parent.style.height = `${target.clientHeight}px`;
+  } catch (e) {}
+}
+
+document.addEventListener('preRequestFullscreen', onPreRequestFullscreen);
+
+function onFullscreenChange(event) {
+  if (document.fullscreenElement) return;
+  try {
+    const target = event.target;
+    const parent = target?.parentElement;
+    if (!parent) return;
+    parent.style.height = '';
+  } catch (e) {}
+}
+
+[
+  'fullscreenchange',
+  'webkitfullscreenchange',
+  'mozfullscreenchange'
+].forEach(name => document.addEventListener(name, onFullscreenChange));
